b14e918686097b9969c1c79c58c3e24323491351
dudrbs703 <dudrbs703@naver.com>
2024-08-05 16:46:10 +0900

    해결한 문제 추가

---
+++ Programmers/코딩 기초 트레이닝/Problem_181909.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Problem_181909
+ */
+public class Problem_181909 {
+
+    public static void main(String[] args) {
+
+        String my_string = "programmers";
+        String[] a = new String[my_string.length()];
+
+        List<String> b = new ArrayList<>();
+        for (int i = 0; i < my_string.length(); i++) {
+            b.add(my_string.substring(i, my_string.length()));
+        }
+
+        Collections.sort(b);
+
+        System.out.println(Arrays.toString(b.stream().toArray(String[]::new)));
+    }
+}
