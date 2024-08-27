575c0a994ffec75c0b5430099c97100d9910980d
dudrbs703 <dudrbs703@naver.com>
2024-08-27 16:03:48 +0900

    해결한 문제 추가

---
+++ Programmers/코딩 기초 트레이닝/Problem_181890.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Problem_181890
+ */
+public class Problem_181890 {
+
+    public static void main(String[] args) {
+        String []str_list = { "l" };
+
+        String []answer = {};
+        for(int i=0; i<str_list.length; i++)
+        {
+            if(str_list[i].equals("l"))
+            {
+                answer = Arrays.copyOfRange(str_list, 0, i);
+                break;
+            }
+
+            if(str_list[i].equals("r"))
+            {
+                answer = Arrays.copyOfRange(str_list, i+1, str_list.length);
+                break;
+            }
+        }
+
+        System.out.println(Arrays.toString(answer));
+        
+    }
+}
