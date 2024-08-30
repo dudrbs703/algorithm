bbcc5aeaa8bec07eef4705b1476790be578fd7bd
dudrbs703 <dudrbs703@naver.com>
2024-08-30 18:57:43 +0900

    해결한 문제 추가

---
+++ Programmers/코딩 기초 트레이닝/Problem_181893.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Problem_181893
+ */
+public class Problem_181893 {
+
+    public static void main(String[] args) {
+        
+        int []arr = {
+            0, 1, 2, 3, 4, 5
+        };
+
+        int []query = {
+            4, 1, 2
+        };
+
+        int []answer = { };
+
+        for(int i : query)
+        {
+
+            answer = new int[arr.length -1];
+
+            for(int j=0; j<answer.length; j++)
+            {
+                if(i == j) continue;
+                else answer[j] = arr[i];
+            }
+
+        }
+
+
+        System.out.println(Arrays.toString(answer));
+    }
+}
