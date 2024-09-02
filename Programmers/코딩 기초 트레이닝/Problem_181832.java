e71d17e7f86f64ae9abb9db3e2888e84ea4365d1
dudrbs703 <dudrbs703@naver.com>
2024-09-02 12:48:05 +0900

    해결한 문제 추가

--- Programmers/코딩 기초 트레이닝/Problem_181832.java
+++ Programmers/코딩 기초 트레이닝/Problem_181832.java
@@ -4,11 +4,15 @@
 public class Problem_181832 {
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 4;
 
         int[][] answer = new int[n][n];
 
+        int size = (int)Math.pow(n, 2);
         int count = 1;
+
+        int row = 0;
+        int column = 0;
 
         int right = 0;
         int left = 1;
@@ -16,48 +20,65 @@ public class Problem_181832 {
         int down = 3;
 
         int direction = right;
-        int row = 0;
-        int column = 0;
 
-        while (count <= n * n) {
+        while (count <= size) {
+
+            if(n == 1)
+            {
+                answer[row][column] = count;
+                break;
+            }
+            
             while (direction == right) {
-                if (column >= n || answer[row][column + 1] != 0) {
+                if (column >= n || answer[row][column] != 0) {
                     column--;
                     direction = down;
                     row++;
                     break;
                 }
+                if( answer[row][column] != 0) break;
                 answer[row][column++] = count++;
             }
 
+            if(count >= size) break;
+
             while (direction == down) {
-                if (row >= n || answer[row + 1][column] != 0) {
+                if (row >= n || answer[row][column] != 0) {
                     row--;
                     direction = left;
                     column--;
                     break;
                 }
+                if( answer[row][column] != 0) break;
                 answer[row++][column] = count++;
             }
 
+            if(count >= size) break;
+
             while (direction == left) {
-                if (column < 0 || answer[row][column - 1] != 0) {
-                    if (column == -1)
-                        column = 0;
-                    column--;
+                if (column == -1) column = 0;
+                if (column < 0 || answer[row][column] != 0) {
                     direction = up;
                     row--;
+                    if(answer[row][column] != 0) column++;
+                    //column++;
                     break;
                 }
                 answer[row][column--] = count++;
             }
+
+            if(count >= size) break;
+
             while (direction == up) {
-                if (row >= n || answer[row - 1][column] != 0) {
-                    row--;
+                if (row < 0 || answer[row][column] != 0) {
+                    row++;
                     direction = right;
                     column++;
                     break;
                 }
+
+                if( answer[row][column] != 0) break;
+
                 answer[row--][column] = count++;
             }
 
