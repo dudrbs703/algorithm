2e12a56f0dbacfbe396f0e228d0c377c1d1fe30a
dudrbs703 <dudrbs703@naver.com>
2024-09-13 00:01:58 +0900

    해결한 문제 추가

---
+++ Programmers/코딩테스트 입문/Problem_120585.java
@@ -0,0 +1,19 @@
+/**
+ * Problem_120585
+ */
+public class Problem_120585 {
+
+    public static void main(String[] args) {
+        int []array = { 149, 180, 192, 170 };
+        int height = 167;
+
+        int answer = 0;
+
+        for(int i : array)
+        {
+            if(i > height) answer++;
+        }
+
+        System.out.println(answer)
+    }
+}
