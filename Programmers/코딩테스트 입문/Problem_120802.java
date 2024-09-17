fe4f4d07074105738cd96751360afeb12acc2f30
dudrbs703 <dudrbs703@naver.com>
2024-09-17 13:18:19 +0900

    해결한 문제 추가

---
+++ Programmers/코딩테스트 입문/Problem_120802.java
@@ -0,0 +1,13 @@
+/**
+ * Problem_120802
+ */
+public class Problem_120802 {
+
+    public static void main(String[] args) {
+        
+        int num1 = 2;
+        int num2 = 3;
+
+        System.out.println(num1 + num2);
+    }
+}
