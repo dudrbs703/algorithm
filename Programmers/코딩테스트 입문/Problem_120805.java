cec3b01d2957c712a188a90eeb7e22fd1de00f9e
dudrbs703 <dudrbs703@naver.com>
2024-09-20 00:52:40 +0900

    해결한 문제 추가

---
+++ Programmers/코딩테스트 입문/Problem_120805.java
@@ -0,0 +1,12 @@
+/**
+ * Problem_120805
+ */
+public class Problem_120805 {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        System.out.println(num1 / num2);
+    }
+}
