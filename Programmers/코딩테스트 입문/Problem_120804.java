f7efcb8e6403da68e7827af2cc80ec18768ec4c9
dudrbs703 <dudrbs703@naver.com>
2024-09-19 17:08:29 +0900

    해결한 문제 추가

---
+++ Programmers/코딩테스트 입문/Problem_120804.java
@@ -0,0 +1,12 @@
+/**
+ * Problem_120804
+ */
+public class Problem_120804 {
+
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 4;
+        
+        System.out.println(num1 * num2);
+    }
+}
