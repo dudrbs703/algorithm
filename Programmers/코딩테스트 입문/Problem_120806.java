ad36473459e144e54e80b3ae2d8f84623e9a8aac
dudrbs703 <dudrbs703@naver.com>
2024-09-23 16:07:04 +0900

    해결한 문제 추가

---
+++ Programmers/코딩테스트 입문/Problem_120806.java
@@ -0,0 +1,16 @@
+/**
+ * Problem_120806
+ */
+public class Problem_120806 {
+
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 3;
+
+        double n1 = num1 * 1.0;
+        double n2 = num2 * 1.0;
+
+        System.out.println((int)((n1/n2) * 1000));
+        //System.out.println((double)(num1/num2));
+    }
+}
