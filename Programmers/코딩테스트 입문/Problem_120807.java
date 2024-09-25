35e87b9dd771025ee0ee4ee486cd95798622f6df
dudrbs703 <dudrbs703@naver.com>
2024-09-25 16:26:55 +0900

    해결한 문제 추가

---
+++ Programmers/코딩테스트 입문/Problem_120807.java
@@ -0,0 +1,12 @@
+/**
+ * Problem_120807
+ */
+public class Problem_120807 {
+
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+
+        System.out.println(num1 == num2 ? 1 : -1);
+    }
+}
