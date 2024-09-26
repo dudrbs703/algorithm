9a1b36ea70264f6293ab7e3d1915576e1a11b6dd
dudrbs703 <dudrbs703@naver.com>
2024-09-27 00:19:13 +0900

    해결한 문제 추가

---
+++ Programmers/코딩테스트 입문/Problem_120810.java
@@ -0,0 +1,12 @@
+/**
+ * Problem_120810
+ */
+public class Problem_120810 {
+
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 2;
+
+        System.out.println(num1 % num2);
+    }
+}
