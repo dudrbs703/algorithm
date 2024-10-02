b0da8343dcdc7bcd463f11aa53a75e8aafbd48d8
dudrbs703 <dudrbs703@naver.com>
2024-10-02 16:58:19 +0900

    해결한 문제 추가

---
+++ Programmers/코딩테스트 입문/Problem_120814.java
@@ -0,0 +1,12 @@
+/**
+ * Problem_120814
+ */
+public class Problem_120814 {
+
+    public static void main(String[] args) {
+        
+        int n = 15;
+
+        System.out.println(n % 7 == 0 ? n / 7 : (n / 7) + 1);
+    }
+}
