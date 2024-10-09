26c28b4391c16d731af8a8a34432caa9409c1750
dudrbs703 <dudrbs703@naver.com>
2024-10-09 15:18:09 +0900

    해결한 문제 추가

---
+++ Programmers/코딩테스트 입문/Problem_120820.java
@@ -0,0 +1,11 @@
+/**
+ * Problem_120820
+ */
+public class Problem_120820 {
+
+    public static void main(String[] args) {
+        int age = 23;
+
+        System.out.println( (2022 - age) + 1);
+    }
+}
