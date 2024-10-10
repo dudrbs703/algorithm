5e7cb792229c1b15a0666d9198057c5a3bebb5f2
dudrbs703 <dudrbs703@naver.com>
2024-10-10 16:08:15 +0900

    해결한 문제 추가

---
+++ Programmers/코딩테스트 입문/Problem_120821.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Problem_120821
+ */
+public class Problem_120821 {
+
+    public static void main(String[] args) {
+        int []num_list = { 1, 2, 3, 4, 5 };
+        int []answer = new int[num_list.length];
+        int position = 0;
+
+        for(int i=num_list.length-1; i>=0; i--)
+        {
+            answer[position++] = num_list[i];
+        }
+
+        System.out.println(Arrays.toString(answer));
+    }
+}
