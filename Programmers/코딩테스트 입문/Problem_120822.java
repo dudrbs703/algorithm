ed0909459a69312eebf9a9c3e7e5328fda0265f2
dudrbs703 <dudrbs703@naver.com>
2024-10-11 17:07:19 +0900

    해결한 문제 추가

---
+++ Programmers/코딩테스트 입문/Problem_120822.java
@@ -0,0 +1,11 @@
+/**
+ * Problem_120822
+ */
+public class Problem_120822 {
+
+    public static void main(String[] args) {
+        String my_string = "jaron";
+
+        System.out.println(new StringBuffer(my_string).reverse().toString());
+    }
+}
