0dd8d3813df4811aa7ffc06347e3d91894295e3a
dudrbs703 <dudrbs703@naver.com>
2024-10-17 16:19:50 +0900

    해결한 문제 추가

---
+++ Programmers/코딩테스트 입문/Problem_120826.java
@@ -0,0 +1,12 @@
+/**
+ * Problem_120826
+ */
+public class Problem_120826 {
+
+    public static void main(String[] args) {
+        String my_string = "abcdef";
+        String letter = "f";
+
+        System.out.println(my_string.replaceAll(letter, ""));
+    }
+}
