dd8df8b5689124adede5494556f0eef05b5756e0
dudrbs703 <dudrbs703@naver.com>
2024-10-21 17:18:05 +0900

    해결한 문제 추가

---
+++ Programmers/코딩테스트 입문/Problem_120830.java
@@ -0,0 +1,15 @@
+/**
+ * Problem_120830
+ */
+public class Problem_120830 {
+
+    public static void main(String[] args) {
+        int n = 10; 
+        int k = 2;
+
+        int service = n / 10;
+
+
+        System.out.println(n*12000+k*2000-service*2000);
+    }
+}
