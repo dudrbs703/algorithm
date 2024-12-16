2f4806ee2b9b3549990690c22e2fb96e3336822d
dudrbs703 <dudrbs703@naver.com>
2024-12-16 16:32:14 +0900

    해결한 문제 추가

---
+++ Programmers/코딩테스트 입문/Problem_120898.java
@@ -0,0 +1,11 @@
+/**
+ * Problem_120898
+ */
+public class Problem_120898 {
+
+    public static void main(String[] args) {
+        String message = "happy birthday!";
+
+        System.out.println(message.length() * 2);
+    }
+}
